3afc6f49a4644a7ab4560f7e35c793a1db25acce
MrWinRock <mrwinrock11@gmail.com>
2025-07-05 15:08:31 +0700

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,20 +5,22 @@ import './../css/navbar.css'
 const Navbar = () => {
 
     return (
-        <nav className="navbar flex items-center justify-between p-4 bg-gray-800 text-white z-10">
-            <div className="flex flex-row items-center justify-center gap-4">
-                <img src={logo} alt="Logo" />
-                <a href="/" className="navbar-title text-[32px] font-[900] text-white">
-                    MrWinRock
-                </a>
-            </div>
-            <div className="hidden flex gap-6 items-center">
-                <Link to="/" className="p-button">Home</Link>
-                <Link to="/about" className="p-button">About</Link>
-                <Link to="/skills" className="p-button">Skills</Link>
-                <Link to="/projects" className="p-button">Projects</Link>
-                <Link to="/experience" className="p-button">Experience</Link>
-                <Link to="/contact" className="p-button">Contact</Link>
+        <nav className="navbar">
+            <div className='flex max-w-[1200px] mx-auto items-center justify-between p-4 bg-gray-800 text-white z-10'>
+                <div className="flex flex-row items-center justify-center gap-4">
+                    <img src={logo} alt="Logo" />
+                    <Link to="/" className="navbar-title text-[32px] sm:text-[40px] font-[900] text-white">
+                        MrWinRock
+                    </Link>
+                </div>
+                <div className=" flex gap-6 items-center">
+                    <Link to="/" className="p-button">Home</Link>
+                    <Link to="/about" className="p-button">About</Link>
+                    <Link to="/skills" className="p-button">Skills</Link>
+                    <Link to="/projects" className="p-button">Projects</Link>
+                    <Link to="/experience" className="p-button">Experience</Link>
+                    <Link to="/contact" className="p-button">Contact</Link>
+                </div>
             </div>
         </nav>
     );
