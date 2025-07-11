ae2dfc0c0f24f8f0b3c44bc6c232772635657f67
MrWinRock <mrwinrock11@gmail.com>
2025-07-11 13:40:27 +0700

    update: skills page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="navbar xl:px-4 ">
+        <nav className="navbar xl:px-4 fixed top-0 left-0 w-full z-50 bg-[#1b1b1b] bg-opacity-95 shadow-md backdrop-blur">
             <div className='flex max-w-[1200px] mx-auto items-center justify-between p-4'>
                 <div className="flex flex-row items-center justify-center gap-1 xl:gap-4">
                     <Link to="/" className="flex flex-row text-[1.5rem] xl:text-[2rem] font-[900] items-center">
@@ -22,7 +22,6 @@ const Navbar = () => {
                         <span className='navbar-title'>MrWinRock</span>
                     </Link>
                 </div>
-
                 {/* Desktop Menu */}
                 <div className="hidden lg:flex lg:gap-4 xl:gap-6 items-center">
                     <Link to="/" className="p-button">Home</Link>
@@ -32,7 +31,6 @@ const Navbar = () => {
                     {/* <Link to="/experience" className="p-button">Experience</Link> */}
                     {/* <Link to="/contact" className="p-button">Contact</Link> */}
                 </div>
-
                 {/* Mobile Menu Button */}
                 <button
                     className="lg:hidden flex flex-col justify-center items-center w-8 h-8 space-y-1 cursor-pointer"
@@ -44,7 +42,6 @@ const Navbar = () => {
                     <span className={`block w-6 h-0.5 bg-white transition-all duration-300 ${isMenuOpen ? '-rotate-45 -translate-y-1.5' : ''}`}></span>
                 </button>
             </div>
-
             {/* Mobile Menu */}
             <div className={`lg:hidden transition-all duration-300 ease-in-out ${isMenuOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0 overflow-hidden'}`}>
                 <div className="flex flex-col gap-4 p-4 width-full">
--- src/components/pages/skills/Skills.tsx
+++ src/components/pages/skills/Skills.tsx
@@ -1,9 +1,38 @@
 const Skills = () => {
     const skills = [
-        { name: 'JavaScript' },
-        { name: 'React' },
-        { name: 'TypeScript' },
-        { name: 'Node.js' },
+        { name: 'Blender', image: 'https://download.blender.org/branding/community/blender_community_badge_white.svg' },
+        { name: 'C', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg' },
+        { name: 'C++', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg' },
+        { name: 'C#', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg' },
+        { name: 'CSS3', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg' },
+        { name: 'Firebase', image: 'https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg' },
+        { name: 'Flutter', image: 'https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg' },
+        { name: 'Heroku', image: 'https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg' },
+        { name: 'Git', image: 'https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg' },
+        { name: 'HTML5', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg' },
+        { name: 'Java', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg' },
+        { name: 'JavaScript', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg' },
+        { name: 'Linux', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg' },
+        { name: 'Photoshop', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg' },
+        { name: 'Python', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg' },
+        { name: 'React', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg' },
+        { name: 'React Native', image: 'https://reactnative.dev/img/header_logo.svg' },
+        { name: 'Unity', image: 'https://www.vectorlogo.zone/logos/unity3d/unity3d-icon.svg' },
+        { name: 'Unreal Engine', image: 'https://raw.githubusercontent.com/kenangundogan/fontisto/036b7eca71aab1bef8e6a0518f7329f13ed62f6b/icons/svg/brand/unreal-engine.svg' },
+        { name: 'Arduino', image: 'https://cdn.worldvectorlogo.com/logos/arduino-1.svg' },
+        { name: 'AWS', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg' },
+        { name: 'Bash', image: 'https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg' },
+        { name: 'Docker', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg' },
+        { name: 'Express', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg' },
+        { name: 'Figma', image: 'https://www.vectorlogo.zone/logos/figma/figma-icon.svg' },
+        { name: 'Google Cloud Platform', image: 'https://www.vectorlogo.zone/logos/google_cloud/google_cloud-icon.svg' },
+        { name: 'Go', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original.svg' },
+        { name: 'MariaDB', image: 'https://www.vectorlogo.zone/logos/mariadb/mariadb-icon.svg' },
+        { name: 'MongoDB', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg' },
+        { name: 'MySQL', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg' },
+        { name: 'Node.js', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg' },
+        { name: 'PostgreSQL', image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg' },
+        { name: 'Postman', image: 'https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg' },
     ];
 
     return (
@@ -12,7 +41,8 @@ const Skills = () => {
                 <h1 className="text-4xl font-bold mb-8">My Skills</h1>
                 <div className="grid md:grid-cols-2 gap-6">
                     {skills.map((skill) => (
-                        <div key={skill.name} className="bg-gray-800 p-6 rounded-lg">
+                        <div key={skill.name} className="p-button p-6 flex flex-row gap-3 items-center justify-center text-center">
+                            <img src={skill.image} alt={skill.name} className="w-8 h-8 rounded-2xl" />
                             <span className="text-lg font-semibold">{skill.name}</span>
                         </div>
                     ))}
